4fd618b0685784fd4ae17e5f3286c0b229ec7b6b
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-09-19 22:31:23 +0100

    backend connection : adding and deleting an employee

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,20 @@ export class EmployeeService {
   private apiServerUrl = '';
   constructor(private http: HttpClient) {}
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(
+      `${this.apiServerUrl}/employee/add`,
+      employee
+    );
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(
+      `${this.apiServerUrl}/employee/delete/${employeeId}`
+    );
   }
 }
